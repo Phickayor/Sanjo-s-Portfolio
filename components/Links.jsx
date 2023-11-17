5c44430966bfea071fcef4a6e636681948a6d487
Olufikayomi Jetawo <71492979+Phickayor@users.noreply.github.com>
2023-11-17 02:00:07 -0800

    Update Links.jsx

--- components/Links.jsx
+++ components/Links.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Links() {
   return (
-    <div className="font-poppins grid grid-cols-3 text-lg md:text-3xl gap-8 md:gap-20 lg:text-4xl text-center py-5 md:py-10">
+    <div className="font-poppins grid grid-cols-3 text-md md:text-3xl gap-8 md:gap-20 lg:text-4xl text-center py-5 md:py-10">
       <div className="relative w-full">
         <div className=" bg-black py-5 md:py-10">
           <a
@@ -17,8 +17,7 @@ function Links() {
       <div className="relative w-full">
         <div className=" bg-black py-5 md:py-10">
           <a
-            href=""
-            download
+            href="https://drive.google.com/file/d/1LcJp3I8v96ygrwdQ__Z0H8h2EfTsjYLK/view?usp=drive_link"
             target="_blank"
             className="absolute bg-white w-full right-3 md:right-5 lg:right-10 bottom-3 md:bottom-6 lg:bottom-8 py-2 md:py-6 lg:py-10"
           >
