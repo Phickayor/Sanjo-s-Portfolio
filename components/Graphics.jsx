2ad46f2aec1b09d177a27fe1af35d5cf78eda924
Olufikayomi Jetawo <71492979+Phickayor@users.noreply.github.com>
2023-11-17 02:03:50 -0800

    Update Graphics.jsx

--- components/Graphics.jsx
+++ components/Graphics.jsx
@@ -13,7 +13,7 @@ function Graphics() {
     "graphics-9"
   ];
   return (
-    <div className="md:py-10 space-y-8">
+    <div className="py-10 space-y-8">
       <div className="mx-auto lg:w-9/12 w-11/12">
         <h1 className="font-blowbrush text-center text-[65px] leading-[70px] md:text-[98px] md:leading-[100px] lg:text-[130px] lg:leading-[130px]">
           Instagram Post Design
